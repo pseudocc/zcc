f2ef989f1354f6789ba285e8e34fc0ab3b20967c
pseudocc <pseudoc@163.com>
2022-03-10 16:08:25 +0800

    align for shrink

--- zvec/zvec_cap.c
+++ zvec/zvec_cap.c
@@ -1,13 +1,17 @@
 #include "./zvec.h"
 
 #define ALLOC_ALIGN_BYTES 4
-void __zvec_grow(zvec_h* this, size_t bytes) {
+static inline size_t ensure_aligned(size_t bytes) {
   size_t remain = remain % ALLOC_ALIGN_BYTES;
-  size_t used = ztype_n_bytes(this->begin, this->end);
-
   if (remain)
     bytes += ALLOC_ALIGN_BYTES - remain;
+  return bytes;
+}
 
+void __zvec_grow(zvec_h* this, size_t bytes) {
+  size_t used = ztype_n_bytes(this->begin, this->end);
+  
+  bytes = ensure_aligned(bytes);
   this->begin = realloc(this->begin, bytes);
   this->end = this->begin + used;
   this->alloc_end = this->begin + bytes;
@@ -17,6 +21,7 @@ void __zvec_shrink(zvec_h* this, size_t bytes) {
   if (this->begin == NULL)
     return;
 
+  bytes = ensure_aligned(bytes);
   this->begin = realloc(this->begin, bytes);
   this->alloc_end = this->begin + bytes;
 }
